1f7c3fe62dc0120a26298272bf58207eb300cfdd
Sergio Baez <sergio.baez231@gmail.com>
2013-03-21 21:37:34 +0100

    primer commit, la neta no se por donde empezar :/

---
+++ src/dlclass/DLHandler.hh
@@ -0,0 +1,16 @@
+/*
+** DLHandler.h for src in /Users/sabs231/Documents/EPITECH/Classes/C++/Projects/Nibbler/nibbler/src
+**
+** Made by sergioandres baezserrano
+** Login   <baezse_s@epitech.net>
+**
+** Started on  Thu Mar 21 21:32:15 2013 sergioandres baezserrano
+** Last update Thu Mar 21 21:34:18 2013 sergioandres baezserrano
+*/
+
+#ifndef DLHANDLER_HH_
+#  define DLHANDLER_HH_
+
+
+
+#endif /* !DLHANDLER_HH_ */

46e78d979a8937eb44bb225e5e1c42dccc8698b8
Sergio Baez <sergio.baez231@gmail.com>
2013-03-22 18:47:01 +0100

    Game class

---
+++ Game.hh
@@ -0,0 +1,39 @@
+/*
+** Game.h for nibbler in /Users/sabs231/Documents/EPITECH/Classes/C++/Projects/Nibbler/nibbler
+**
+** Made by sergioandres baezserrano
+** Login   <baezse_s@epitech.net>
+**
+** Started on  Fri Mar 22 18:37:56 2013 sergioandres baezserrano
+** Last update Fri Mar 22 18:38:03 2013 sergioandres baezserrano
+*/
+
+#ifndef GAME_HH_
+#  define GAME_HH_
+
+#  inlcude  "Field.hh"
+#  include  "Surface.hh"
+#  include  "Food.hh"
+#  include  "Snake .hh"
+
+class Game
+{
+  protected:
+    Field       field;
+    Surface     surface;
+    Snake       snake;
+    Food        food;
+  public:
+    Game();
+    Game(const int width, const int heigth);
+    Game(const Game &_other);
+    virtual ~Game();
+    bool            left(void);
+    bool            right(void);
+    bool            move(void);
+    void            quit(void);
+    field_object_T  get_field(void);
+    void            regen_food(void);
+};
+
+#endif /* !GAME_HH_ */
